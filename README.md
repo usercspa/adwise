ae416061f0bdab70cd507b7bf63bf0c8714d663a
usercspa <spacchbk@gmail.com>
2020-11-27 22:42:53 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# adwiseapp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/adwiseapp)
---
+++ script.js
@@ -0,0 +1,42 @@
+console.log('hello!')
+// Import stylesheets
+import './style.css';
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+import * as firebaseui from 'firebaseui';
+
+// Document elements
+const form = document.getElementById('leave-message');
+const input = document.getElementById('message');
+
+async function main() {
+
+  // Add Firebase project configuration object here
+  // var firebaseConfig = {};
+
+  // firebase.initializeApp(firebaseConfig);
+
+  // FirebaseUI config
+  const uiConfig = {
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+    signInOptions: [
+      // Email / Password Provider.
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        // Handle sign-in.
+        // Return false to avoid redirect.
+        return false;
+      }
+    }
+  };
+
+  // const ui = new firebaseui.auth.AuthUI(firebase.auth());
+}
+main();
